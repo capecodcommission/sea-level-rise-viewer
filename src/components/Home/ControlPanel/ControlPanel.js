e5fdc037e4c8b2585fe353cf88112a86dadd1b03
Mario Carloni <mario.carloni@capecodcommission.org>
2019-04-08 15:32:28 -0400

    control panel now transitions open and closed using conditional css styling
    control panel now slides in and out on-click of toggle button or if loading slr interaction
    
    refactored css class ternary operator

--- src/components/Home/ControlPanel/ControlPanel.js
+++ src/components/Home/ControlPanel/ControlPanel.js
@@ -4,16 +4,48 @@ import BaseMapSwitcher from './BaseMapSwitcher/BaseMapSwitcher'
 import Layers from './Layers/Layers'
 import Title from './Title/Title'
 import {observer} from 'mobx-react'
-import {Row, Col, Grid} from 'react-bootstrap'
+import {Row, Col, Grid, Button} from 'react-bootstrap'
 import css from './ControlPanel.css'
+import RootStore from '../../../store'
 
 @observer
 class ControlPanel extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      panelButtonOpenness: false,
+    }
+  }
+
+  togglePanel = () => {
+    this.setState(prevState => ({
+      panelButtonOpenness: !prevState.panelButtonOpenness,
+    }))
+  }
+
   // Render all control panel sub-components in Bootstrap grid
   render = () => {
     return (
       <Grid>
-        <Row className={css.ControlsWrapper}>
+        <Row 
+          className={
+            (this.state.panelButtonOpenness && RootStore.EsriMapStore.loadingComplete) 
+              ? css.ControlsWrapperOpen 
+              : css.ControlsWrapperClosed
+          }
+        >
+          <Button
+            className = "btn btn-primary" 
+            style = {{
+              float:'right', 
+              position: 'absolute', 
+              display: 'inline-block', 
+              zIndex: '10', 
+              left: '275px',
+            }} 
+            onClick = {this.togglePanel.bind(this)}>-->
+          </Button>
           <Col>
             <Title />
             <BaseMapSwitcher />
