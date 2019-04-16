b7ade4b7bace925889e823e6bad88c09c903fda4
Shawn <shawn.goulet@gmail.com>
2019-04-16 13:47:56 -0400

    updated legend as best as possible without completely re-engineering less bootstrap.

--- src/components/Home/Legend/Legend.js
+++ src/components/Home/Legend/Legend.js
@@ -11,13 +11,13 @@ class Legend extends Component {
   render = () => {
     let townLinesLegend = (
       <Row style={{paddingTop: '1em', paddingBottom: '1em'}}>
-        <Col xs={3} md={3} lg={3}>
+        <Col xs={3} sm={3} md={3} lg={3}>
           <div
             style={{backgroundColor: '#7e8b9e'}}
             className={css.colorBar}
           />
         </Col>
-        <Col xs={9} md={9} lg={9}>
+        <Col xs={9} sm={9} md={9} lg={9}>
           <strong>TOWN LINES</strong>
         </Col>
       </Row>
@@ -25,10 +25,10 @@ class Legend extends Component {
 
     let disConRoadsLegend = (
       <Row style={{paddingTop: '1em'}}>
-        <Col xs={3} md={3} lg={3}>
+        <Col xs={3} sm={3} md={3} lg={3}>
           <div style={{backgroundColor: 'red'}} className={css.colorBar} />
         </Col>
-        <Col xs={9} md={9} lg={9}>
+        <Col xs={9} sm={9} md={9} lg={9}>
           <strong>DISCONNECTED ROADS</strong>
         </Col>
       </Row>
@@ -36,31 +36,26 @@ class Legend extends Component {
 
     let lowLyingAreasLegend = (
       <Row style={{paddingTop: '1em', paddingBottom: '1em'}}>
-        <Col xs={3} md={3} lg={3}>
+        <Col xs={3} sm={3} md={3} lg={3}>
           <div
             style={{backgroundColor: '#63cc70'}}
             className={css.colorBar}
           />
         </Col>
-        <Col xs={9} md={9} lg={9}>
+        <Col xs={9} sm={9} md={9} lg={9}>
           <strong>LOW LYING AREAS</strong>
         </Col>
       </Row>
     )
 
     let critFacLegend = (
-      <div>
-        <Row style={{paddingTop: '1em'}}>
-          <Col xs={3} md={3} lg={3}>
-            <Image
-              src={require('../ControlPanel/Layers/img/critFac.svg')}
-              className={css.critFac}
-            />
-          </Col>
-          <Col xs={9} md={9} lg={9}>
-            <strong>CRITICAL FACILITIES</strong>
-          </Col>
-        </Row>
+      <div className={css.imgTextFix}>
+        <Image
+          src={require('../ControlPanel/img/Icon-Critical-Facilities.png')}
+          className={css.critFac}
+          circle
+        />
+        <span><strong>CRITICAL FACILITIES</strong></span>
       </div>
     )
 
@@ -70,57 +65,57 @@ class Legend extends Component {
           <strong className={css.legendSubTitles}>SLOSH</strong>
         </Row>
         <Row style={{paddingTop: '0.25em'}}>
-          <Col xs={3} md={3} lg={3}>
+          <Col xs={3} sm={3} md={3} lg={3}>
             <div
               style={{backgroundColor: '#ff0000'}}
               className={css.colorBar}
             />
           </Col>
-          <Col xs={9} md={9} lg={9}>
+          <Col xs={9} sm={9} md={9} lg={9}>
             <strong>CATEGORY 1</strong>
           </Col>
         </Row>
         <Row style={{paddingTop: '0.25em'}}>
-          <Col xs={3} md={3} lg={3}>
+          <Col xs={3} sm={3} md={3} lg={3}>
             <div
               style={{backgroundColor: '#ff3411'}}
               className={css.colorBar}
             />
           </Col>
-          <Col xs={9} md={9} lg={9}>
+          <Col xs={9} sm={9} md={9} lg={9}>
             <strong>CATEGORY 2</strong>
           </Col>
         </Row>
         <Row style={{paddingTop: '0.25em'}}>
-          <Col xs={3} md={3} lg={3}>
+          <Col xs={3} sm={3} md={3} lg={3}>
             <div
               style={{backgroundColor: '#ff6823'}}
               className={css.colorBar}
             />
           </Col>
-          <Col xs={9} md={9} lg={9}>
+          <Col xs={9} sm={9} md={9} lg={9}>
             <strong>CATEGORY 3</strong>
           </Col>
         </Row>
         <Row style={{paddingTop: '0.25em'}}>
-          <Col xs={3} md={3} lg={3}>
+          <Col xs={3} sm={3} md={3} lg={3}>
             <div
               style={{backgroundColor: '#ff9c35'}}
               className={css.colorBar}
             />
           </Col>
-          <Col xs={9} md={9} lg={9}>
+          <Col xs={9} sm={9} md={9} lg={9}>
             <strong>CATEGORY 4</strong>
           </Col>
         </Row>
         <Row style={{paddingTop: '0.25em'}}>
-          <Col xs={3} md={3} lg={3}>
+          <Col xs={3} sm={3} md={3} lg={3}>
             <div
               style={{backgroundColor: '#ffd047'}}
               className={css.colorBar}
             />
           </Col>
-          <Col xs={9} md={9} lg={9}>
+          <Col xs={9} sm={9} md={9} lg={9}>
             <strong>CATEGORY 5</strong>
           </Col>
         </Row>
@@ -133,24 +128,24 @@ class Legend extends Component {
           <strong className={css.legendSubTitles}>FEMA</strong>
         </Row>
         <Row>
-          <Col xs={3} md={3} lg={3}  style={{paddingTop: '0.25em'}}>
+          <Col xs={3} sm={3} md={3} lg={3}  style={{paddingTop: '0.25em'}}>
             <div
               style={{backgroundColor: '#f1a800'}}
               className={css.colorBar}
             />
           </Col>
-          <Col xs={9} md={9} lg={9} style={{paddingTop: '0.25em'}} >
+          <Col xs={9} sm={9} md={9} lg={9} style={{paddingTop: '0.25em'}} >
             <strong>SPECIAL FLOOD HAZARD AREAS</strong>
           </Col>
         </Row>
         <Row>
-          <Col xs={3} md={3} lg={3} style={{paddingTop: '0.25em'}} >
+          <Col xs={3} sm={3} md={3} lg={3} style={{paddingTop: '0.25em'}} >
             <div
               style={{backgroundColor: '#d20000'}}
               className={css.colorBar}
             />
           </Col>
-          <Col xs={9} md={9} lg={9} style={{paddingTop: '0.25em'}} >
+          <Col xs={9} sm={9} md={9} lg={9} style={{paddingTop: '0.25em'}} >
             <strong>COASTAL HIGH HAZARD AREAS</strong>
           </Col>
         </Row>
@@ -158,17 +153,14 @@ class Legend extends Component {
     )
 
     let intersectLegend = (
-      <Row style={{paddingTop: '1em'}}>
-        <Col xs={3} md={3} lg={3}>
-          <Image
-            src={require('../ControlPanel/Layers/img/critFac.svg')}
-            className={css.affectedCritFac}
-          />
-        </Col>
-        <Col xs={9} md={9} lg={9}>
-          <strong>AFFECTED CRITICAL FACILITIES</strong>
-        </Col>
-      </Row>
+      <div className={css.imgTextFix}>
+        <Image
+          src={require('../ControlPanel/img/Icon-Critical-Facilities.png')}
+          className={css.affectedCritFac}
+          circle
+        />
+        <span><strong>AFFECTED CRIT FACs</strong></span>
+      </div>
     )
 
     let legendTable = (
@@ -186,11 +178,11 @@ class Legend extends Component {
               <strong className={css.legendTitle}>LEGEND</strong>
             </Row>
             <Row>
-              <Col xs={6} md={6} lg={6}>
+              <Col xs={6} sm={6} md={6} lg={6}>
                 {townLinesLegend}
                 {RootStore.ControlPanelStore.sloshBackground ? sloshLegend : null}
               </Col>
-              <Col xs={6} md={6} lg={6}>
+              <Col xs={6} sm={6} md={6} lg={6}>
                 {RootStore.ControlPanelStore.currentSliderValue > 0
                   ? disConRoadsLegend
                   : null}
